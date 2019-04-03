542998ac1c5dc595809f5cbcc92c504bf70516a9
zbory <zborayb@gmail.com>
2019-04-03 17:24:42 +0200

    MileToKmConverter

---
+++ week-01/day-3/MileToKmConverter/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+    int kilometer;
+
+    std::cout << "Please input the distance in kilometers: " << std::endl;
+    std::cin >> kilometer;
+    std::cout << "The distance in miles is " << kilometer * 1.61 << "." << std::endl;
+
+    return 0;
+}
