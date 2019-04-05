615f144398c730d5cc79352adda918a377d8a264
zbory <zborayb@gmail.com>
2019-04-05 17:11:28 +0200

    ConditionalVariableMutation: isBonus task fixed

--- week-01/day-3/ConditionalVariableMutation/main.cpp
+++ week-01/day-3/ConditionalVariableMutation/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *args[]) {
 
     if (credits >= 50 && !isBonus)
         c -= 2;
-    else if (!isBonus)
+    if (credits < 50 && !isBonus)
         c -= 1;
 
     std::cout << c << std::endl;
