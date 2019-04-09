45f8a9db1a9225a8c00612b14af8e763744a509c
zbory <zborayb@gmail.com>
2019-04-09 11:39:36 +0200

    TakesLonger finished

---
+++ week-02/day-2/TakesLonger/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string quote("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
+
+    // When saving this quote a disk error has occured. Please fix it.
+    // Add "always takes longer than" to the quote between the words "It" and "you" using the replace function
+
+    quote.replace(20, 1, " always takes longer than ");
+
+    std::cout << quote << std::endl;
+    return 0;
+}
