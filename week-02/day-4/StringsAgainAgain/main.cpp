33a775e97f4a1b953ca9be0914546a153b977e56
zbory <zborayb@gmail.com>
2019-04-11 13:20:17 +0200

    StringsAgainAgain comment added

--- week-02/day-4/StringsAgainAgain/main.cpp
+++ week-02/day-4/StringsAgainAgain/main.cpp
@@ -15,6 +15,7 @@ void recursiveChangeX(std::string &string, std::string &result)
 {
     if (string.size() == 0) return;
 
+    //if-else selection to not put * before first character (iterating through string backwards)
     if (string.size() != 1) {
         result.insert(0, 1, string.at(string.size() - 1));
         result.insert(0, "*");
