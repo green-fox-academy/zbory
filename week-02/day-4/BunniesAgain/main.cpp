6f42f7bc4c654b024178088ff231a0c8069295b9
zbory <zborayb@gmail.com>
2019-04-11 12:42:52 +0200

    BunniesAgain finished

---
+++ week-02/day-4/BunniesAgain/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int recursiveBunnyEars(int n);
+
+int main()
+{
+    std::cout << recursiveBunnyEars(2) << std::endl;
+    return 0;
+}
+
+int recursiveBunnyEars(int n)
+{
+
+    if (n == 1) return 2;
+
+    if (n % 2)
+        return 2 + recursiveBunnyEars(n - 1);
+    else
+        return 3 + recursiveBunnyEars(n - 1);
+}
