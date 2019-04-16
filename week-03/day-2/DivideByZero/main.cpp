239e8d9c11321f69a873ed8fa57dfe3868512c30
zbory <zborayb@gmail.com>
2019-04-16 11:44:15 +0200

    DivideByZero finished

---
+++ week-03/day-2/DivideByZero/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    // Create a function that takes a number
+    // divides ten with it,
+    // and prints the result.
+    // It should print "fail" if the parameter is 0
+
+    int number;
+    std::cout << "Please input a divisor:";
+    std::cin >> number;
+
+    try{
+        if(number == 0){
+            throw std::runtime_error("fail");
+        }
+        std::cout << 10 / number;
+    }catch(std::runtime_error e){
+        std::cout << e.what();
+    }
+
+    return 0;
+}
