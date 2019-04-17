6e667393698689287872ad9cf136be029d495825
zbory <zborayb@gmail.com>
2019-04-17 15:28:33 +0200

    FleetOfThings finished

--- week-03/day-3/FleetOfThings/main.cpp
+++ week-03/day-3/FleetOfThings/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int main()
+#include "fleet.h"
+
+int main(int argc, char *args[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    Fleet fleet;
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+
+    Thing t1("Stand up");
+    Thing t2("Eat lunch");
+
+    t1.complete();
+    t2.complete();
+
+    fleet.add(Thing("Get milk"));
+    fleet.add(Thing("Remove the obstacles"));
+    fleet.add(t1);
+    fleet.add(t2);
+    
+    fleet.toString();
+
+    std::cout << fleet.toString() << std::endl;
     return 0;
 }
