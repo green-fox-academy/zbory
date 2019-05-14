a2ca2a4d279e1a9af2f2ca06006f3b0a99452fa8
zbory <zborayb@gmail.com>
2019-05-14 11:48:51 +0200

    Implement the HelloOthers exercise

---
+++ week-06/day-2/HelloOthers/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, Sanyi!\nHello, Anna!\nHello, Szucsi!");
+    return 0;
+}
