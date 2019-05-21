675ac2ce8f3f8b0beb4510dfbf17d5fbd67fa4ee
zbory <zborayb@gmail.com>
2019-05-21 14:38:22 +0200

    Implement the BlockingTheExecution exercise

---
+++ week-07/day-2/BlockingTheExecution/main.c
@@ -0,0 +1,32 @@
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+int main(void)
+{
+	HAL_Init();
+
+	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+
+	BSP_LED_Init(LED1);
+
+	int state = 0;
+	int button_down = 0;
+
+	while (1) {
+		/* If button is pressed do something */
+		if (BSP_PB_GetState(BUTTON_KEY) == 1 && button_down == 0) {
+			state = 1;
+			button_down = 1;
+		}
+		if (BSP_PB_GetState(BUTTON_KEY) == 0) {
+			button_down = 0;
+		}
+		if (state) {
+			BSP_LED_On(LED1);
+			HAL_Delay(250);
+			BSP_LED_Off(LED1);
+			HAL_Delay(250);
+		}
+	}
+}
+
